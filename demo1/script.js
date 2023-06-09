dc8876bf17305ccee81330f85754eb93adaa0d3d
hchiam <hchiam@users.noreply.github.com>
2023-06-09 14:41:19 -0600

    demo1

---
+++ demo1/script.js
@@ -0,0 +1,19 @@
+// import { Font } from '@ckeditor/ckeditor5-font';
+// import Font from '@ckeditor/ckeditor5-font';
+// import Font from '@ckeditor/ckeditor5-font/src/font'
+// import FontColor from '@ckeditor/ckeditor5-font/src/fontcolor';
+
+let ckeditor;
+ClassicEditor.create(document.querySelector("#editor"), {
+  // plugins: [ Font ],
+  // toolbar: [ 'fontColor' ], // uncommenting this breaks things and throws toolbarview-item-unavailable error
+})
+  .then((editor) => {
+    // console.log(Array.from(
+    //   editor.ui.componentFactory.names()
+    // ));
+    ckeditor = editor;
+  })
+  .catch((error) => {
+    console.error(error);
+  });
